cbe35ee81a07837c83d8a36d1b75a871c286f34a
bangbang1232 <38347730+bangbang1232@users.noreply.github.com>
2018-10-30 01:01:37 -0500

    Add files via upload

---
+++ Js and Jquey/JavaScript.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+	$(".suppIn").fadeOut()
+	$(".distIn").fadeOut()
+	$(".supp").click(function () {
+		$(".dist").animate({
+			height: 'toggle'
+		});
+		$(".suppIn").fadeToggle()
+	});
+	$(".dist").click(function () {
+		$(".supp").animate({
+			height: 'toggle'
+		});
+		$(".distIn").fadeToggle()
+	});
+});
+
